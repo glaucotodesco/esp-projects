566a18c6a246ee599df151cebc0e97dcab370573
Glauco Todesco <glauco.todesco@gmail.com>
2024-12-15 14:16:52 -0300

    Hello Oled

---
+++ Hello-Oled/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+
+#include <Wire.h>
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void setup() {
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+  { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,00);
+  // Display static text
+  display.println("Glauco Todesco");
+  
+  display.setTextSize(2);
+  display.setCursor(0,30);
+  display.println("Glauco");
+  display.setCursor(0,50);
+  display.println("Todesco");
+  display.display(); 
+  delay(100);
+}
+void loop() {
+  // // Scroll in various directions, pausing in-between:
+  display.startscrollright(0x00, 0x0F);
+  delay(7000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(7000);
+  display.stopscroll();
+  delay(1000);
+}
--- README.md
+++ README.md
@@ -10,5 +10,11 @@
 # Monitor
 - Type: pio device monitor
 
+# Kit used
+![Kit](Kit.JPG "Título da imagem")
 
+# Project Reference
+![Projetos de Referência](https://github.com/LontenTechnology/ESP32_Basic_Starter_Kit_LTARK_8 "Projetos de Referência")
 
+# Tutorials
+![Tutorial](StarterKit.pdf "Tutoriais")
